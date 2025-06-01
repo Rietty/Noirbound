4f427beadda961c2b69ddffc4cd1269c57d66870
Rietty <rie.juliette@gmail.com>
2025-06-01 00:02:39 -0400

    Lowercase for gamestate

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- Noirbound: Main Entrypoint
 
 -- Library imports:
-local Gamestate = require "lib.external.hump.gamestate"
+local gamestate = require "lib.external.hump.gamestate"
 
 -- Game states:
 local menu      = require "states.menu"
@@ -13,8 +13,8 @@ local credits   = require "states.credits"
 -- Initalize the game.
 function love.load()
     -- Manage game states with hump.gamestate:
-    Gamestate.registerEvents() -- Register love events with Gamestate
-    Gamestate.switch(menu)     -- Start with the menu state
+    gamestate.registerEvents() -- Register love events with Gamestate
+    gamestate.switch(menu)     -- Start with the menu state
 end
 
 -- We don't need to use love.update or love.draw, as hump.gamestate will handle that for us.
