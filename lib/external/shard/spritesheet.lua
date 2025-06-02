386af85f749a816f147e6e4c97dbf5624f838c83
Rietty <rie.juliette@gmail.com>
2025-06-01 20:22:05 -0400

    Get a border working for UI

---
+++ lib/external/shard/spritesheet.lua
@@ -0,0 +1,50 @@
+--[[
+MIT License
+
+Copyright (c) 2019 MineGame159
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]--
+
+local spritesheet = {}
+spritesheet._mt = {
+    __index = spritesheet
+}
+
+function spritesheet:new(filename)
+    local instance = {}
+    setmetatable(instance, spritesheet._mt)
+
+    local config = love.filesystem.load(filename)()
+    instance.image = love.graphics.newImage(config.image)
+    instance.image:setFilter(config.filter or "nearest", config.filter or "nearest")
+    instance.quads = {}
+
+    for i, v in pairs(config.sprites) do
+        instance.quads[i] = love.graphics.newQuad(v.x or 0, v.y or 0, v.w or 0, v.h or 0, instance.image:getDimensions())
+    end
+
+    return instance
+end
+
+function spritesheet:draw(name, x, y, r, sx, sy, ox, oy)
+    love.graphics.draw(self.image, self.quads[name] or nil, x or 0, y or 0, r or 0, sx or 1, sy or 1, ox or 0, oy or 0)
+end
+
+return spritesheet
--- states/menu.lua
+++ states/menu.lua
@@ -3,6 +3,7 @@ local gamestate = require "lib.external.hump.gamestate"
 local prologue = require "states.prologue"
 local music = require "lib.internal.audio.bg"
 local grid = require "lib.internal.utils.grid"
+local sprites = require "lib.external.shard.spritesheet"
 
 local menu = {}
 
@@ -10,9 +11,19 @@ function menu:enter()
     -- Font:
     self.titleFont = love.graphics.newFont("assets/fonts/Direct_Message.ttf", 72)
     self.subtitleFont = love.graphics.newFont("assets/fonts/Direct_Message.ttf", 24)
+    self.fontAscent = self.titleFont:getAscent()
+    self.fontHeightPadding = 6
 
     -- Music:
     music.play("assets/sfx/bg5.wav", true, true)
+
+    -- Grid:
+    self.gridSize = 32
+    self.gameWidth = love.graphics.getWidth()
+    self.gameHeight = love.graphics.getHeight()
+
+    -- Load sprites:
+    self.frames = sprites:new("assets/sprites/ui/frames.lua")
 end
 
 function menu:update(dt)
@@ -25,15 +36,30 @@ function menu:draw()
     love.graphics.clear(0, 0, 0)
 
     -- Draw the grid:
-    local gridSize = 32
-    grid:draw(gridSize)
+    -- grid:draw(self.gridSize)
 
-    -- Draw menu text:
+    -- Draw title text:
     love.graphics.setColor(1, 1, 1) -- Reset color to white
+
     love.graphics.setFont(self.titleFont)
-    love.graphics.printf("NOIRBOUND", 0, 0, love.graphics.getWidth(), "center")
-    love.graphics.setFont(self.subtitleFont)
-    love.graphics.printf("Press Enter to start", 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), "center")
+    love.graphics.print("NOIRBOUND", self.gridSize * 2.5, self.gridSize * 1.5 + self.gridSize - self.fontAscent - self.fontHeightPadding)
+
+    -- Draw the border lines using the correct frames:
+    for x = 32, self.gameWidth - 48, 8 do
+        self.frames:draw("border_solid_top", x, 32, 0, 2, 2)
+        self.frames:draw("border_solid_bottom", x, 112, 0, 2, 2)
+    end
+
+    for y = 32, 112, 8 do
+        self.frames:draw("border_solid_left", 32, y, 0, 2, 2)
+        self.frames:draw("border_solid_right", self.gameWidth - 48, y, 0, 2, 2)
+    end
+
+    -- Draw the UI frames and outlines:
+    self.frames:draw("corner_box_top_left", 32, 32, 0, 2, 2)
+    self.frames:draw("corner_box_top_right", self.gameWidth - 48, 32, 0, 2, 2)
+    self.frames:draw("corner_box_bottom_left", 32, 112, 0, 2, 2)
+    self.frames:draw("corner_box_bottom_right", self.gameWidth - 48, 112, 0, 2, 2)
 end
 
 function menu:keypressed(key)
