b951936e7c9494e45b46df38b33d606b8d696818
Rietty <rie.juliette@gmail.com>
2025-06-03 20:12:15 -0400

    Minor fix to tiny.lua

--- lib/external/ecs/tiny.lua
+++ lib/external/ecs/tiny.lua
@@ -19,7 +19,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
 
---- @module tiny-ecs
+-- @module tiny-ecs
 -- @author Calvin Rose
 -- @license MIT
 -- @copyright 2016
